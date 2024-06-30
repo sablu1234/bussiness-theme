f5deb25fbf600976bc2750f903a3dd7b4a4851c1
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-06-30 22:40:52 +0600

    themefile2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+// $home =0;
+// while($home<10){
+//     echo $home;
+//     $home++;
+// }
+$all_post= wp_count_posts()->publish;
+echo $all_post;
+// echo wp_count_posts()->publish;
+
+
+
+$moga=$all_post;
+while($moga<10){
+    echo $moga;
+    $moga++;
+}
+?>
