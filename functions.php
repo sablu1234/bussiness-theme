06bbeb409230083677d3ff636d7ee7b42d4be16f
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-07-06 19:33:35 +0600

    bussiness

--- functions.php
+++ functions.php
@@ -1,39 +1,15 @@
 <?php
-// hooks 2 typs
-// add_action();
-// add_filter();
 
-
-
-// if(!function_exists('mytheme')){
-//     function mytheme(){
-//         add_theme_support( 'post-thumbnails' );
-//         add_theme_support( 'post-formats', array( 'aside', 'gallery', 'quote', 'image', 'video' ) );
-//     }
-// }
-// add_action('after_setup_theme','mytheme');
-
-
-
-
-
-// if(!function_exists('mtyheme')){
-//     function mytheme(){
-//         add_theme_support('post-thumbnails');
-//     }
-// }
-// add_action('after_setup_theme','mytheme')
-
-
-
-
-
-
-if(!function_exists('mtyheme')){
+if(!function_exists('mytheme')){
     function mytheme(){
-        add_theme_support('post-thumbnails');
+    add_theme_support('post-thumbnails');
     }
-}
-add_action('after_setup_theme','mytheme')
 
+    function change_featured_image_text( $conten ) {
+        return $conten = str_replace( __( 'Set featured image' ), __( 'hasan post thumbnail' ), $conten);
+    }
+    add_filter( 'admin_post_thumbnail_html', 'change_featured_image_text' );
+
+}
+add_action('after_setup_theme','mytheme');
 ?>
---
+++ single.php
@@ -0,0 +1,7 @@
+<?php
+echo "allah mohan";
+the_content();
+
+
+
+?>
